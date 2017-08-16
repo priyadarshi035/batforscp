f7114d11035e20f797e9677b161819b26a1d2f65
priyadarshi035 <priypete@in.ibm.com>
2017-08-16 16:45:18 +0530

    Update db_backup_artifactory.bat

--- db_backup_artifactory.bat
+++ db_backup_artifactory.bat
@@ -1,5 +1,4 @@
-del backup_script_called.txt
-echo.>"C:\Users\cloud_user\backup_script_called.txt"
+@echo Writing text to dblank.txt>check1.txt
 waitfor StartBatchCommand /t 20
 SET DatabaseBackupPath=D:\db\dbbackup
 
@@ -7,12 +6,12 @@ FOR /F "delims=|" %%I IN ('DIR "%DatabaseBackupPath%\*.001" /B /O:D') DO SET  Ne
 echo %NewestFile%
 set Pathname="D:\db\dbbackup"
 cd /d %Pathname%
-
+@echo Writing text to dblank.txt>check2.txt
 
 :loop
 FOR %%A IN (%NewestFile%) DO SET fileSize=%%~zA
 echo 'file size is ' %fileSize%
-
+@echo Writing text to dblank.txt>check3.txt
 set /A COUNTER=COUNTER+1
 echo "counter value---->"%COUNTER%
 if %COUNTER% EQU 1 (
@@ -20,22 +19,25 @@ if %COUNTER% EQU 1 (
 )
 
 echo 'temp is' %temp1%
-
+@echo Writing text to dblank.txt>check4.txt
 if %fileSize% GTR %temp1% (
    echo File is greater than %minbytesize% bytes 
    set /A temp1=%fileSize%
    waitfor click /t 120
    echo peter
-   goto :loop 
+   goto :loop
+@echo Writing text to dblank.txt>check5.txt 
 ) else (
     echo File is ended
     echo %DatabaseBackupPath%
     echo %NewestFile%
+@echo Writing text to dblank.txt>check6.txt
     FOR /F "delims=|" %%I IN ('DIR "%DatabaseBackupPath%\*.001" /B /O:D') DO SET  NewestFile=%%I
-    curl -k -i -H "X-JFrog-Art-Api: " -X PUT  
+    curl -k -i -H "X-JFrog-Art-Api: AKCp5Z2hL6AoTZbcqMeEbXZvJoxcB9AxyvCB979anTH83VViM9jKhaazoav8H4hNLtZKWVciW" -X PUT  
 
-"https://gbsartif/artifactory/ScopePlus/%1-db_backup_Maverick/%NewestFile%" -T %NewestFile%
-    echo Build_Number %1
+"https://gbsartifactory.in.edst.ibm.com/artifactory/ScopePlus/%1-db_backup_Maverick/%NewestFile%" -T %NewestFile%
+@echo Writing text to dblank.txt>check7.txt    
+echo Build_Number %1
     echo response is %errorlevel%
     IF %errorlevel% NEQ 0 GOTO ERROR_HANDLER
     
@@ -44,7 +46,8 @@ echo "File processing ended"
 goto :exitloop
 :ERROR_HANDLER
 echo ERROR HANDLER GETTING EXECUTED
-curl -X POST http://9.74:8080/view/Maverick_DBDeploy/job/Maverick_ArtifactoryNotify/buildWithParameters?Error_Message=Artifactory_Backup_error --user 
+@echo Writing text to dblank.txt>check8.txt
+curl -X POST http://9.193.198.74:8080/view/Maverick_DBDeploy/job/Maverick_ArtifactoryNotify/buildWithParameters?Error_Message=Artifactory_Backup_error --user 
 
-admin:
+admin:3660e902574e02e6db8f29058cc6cf5e
 :exitloop
