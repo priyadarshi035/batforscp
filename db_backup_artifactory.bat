9ad87600822de5cf8c87d510ffe57c516d9afcd9
priyadarshi035 <priypete@in.ibm.com>
2017-08-16 14:09:21 +0530

    Update db_backup_artifactory.bat

--- db_backup_artifactory.bat
+++ db_backup_artifactory.bat
@@ -30,7 +30,7 @@ if %fileSize% GTR %temp1% (
     echo %DatabaseBackupPath%
     echo %NewestFile%
     FOR /F "delims=|" %%I IN ('DIR "%DatabaseBackupPath%\*.001" /B /O:D') DO SET  NewestFile=%%I
-    curl -X POST http://9.193.198.74:8080/view/Maverick_DBDeploy/job/ScoePlus-StoreArtifacts/buildWithParameters?NewestFile=%NewestFile% --user admin:3660e902574e02e6db8f29058cc6cf5e
+    curl -X POST 
 )
 echo "File processing ended"
 :exitloop
