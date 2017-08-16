acc8067a68cf566a5286829403ea89a88ff4b5eb
priyadarshi035 <priypete@in.ibm.com>
2017-08-16 15:51:24 +0530

    Update db_backup_artifactory.bat

--- db_backup_artifactory.bat
+++ db_backup_artifactory.bat
@@ -30,7 +30,15 @@ if %fileSize% GTR %temp1% (
     echo %DatabaseBackupPath%
     echo %NewestFile%
     FOR /F "delims=|" %%I IN ('DIR "%DatabaseBackupPath%\*.001" /B /O:D') DO SET  NewestFile=%%I
-    curl -X POST 
+    curl -k -i -H "X-JFrog-Art-Api: AKCp5Z2hL6AoTZbcqMeEbXZvJoxcB9AxyvCB979anTH83VViM9jKhaazoav8H4hNLtZKWVciW" -X PUT  "https://gbsartifactory.in.edst.ibm.com/artifactory/ScopePlus/%1-db_backup_Maverick/%NewestFile%" -T %NewestFile%
+    echo Build_Number %1
+    echo response is %errorlevel%
+    IF %errorlevel% NEQ 0 GOTO ERROR_HANDLER
+    
 )
 echo "File processing ended"
+goto :exitloop
+:ERROR_HANDLER
+echo ERROR HANDLER GETTING EXECUTED
+curl -X POST http://9.193.198.74:8080/view/Maverick_DBDeploy/job/Maverick_ArtifactoryNotify/buildWithParameters?Error_Message=Artifactory_Backup_error --user admin:3660e902574e02e6db8f29058cc6cf5e
 :exitloop
